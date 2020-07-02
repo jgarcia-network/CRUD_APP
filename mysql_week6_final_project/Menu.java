ac7382d94d8b6e7979b9f538fceb306eb5a8c485
jgarcia-network <3106189+jgarcia-network@users.noreply.github.com>
2020-07-01 18:26:42 -0700

    Update Menu.java

--- mysql_week6_final_project/Menu.java
+++ mysql_week6_final_project/Menu.java
@@ -2,6 +2,7 @@ package CRUD_APP;
 
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,7 +14,7 @@ public class Menu {
 	private int carIndex = 1;
 	
 	private Scanner scanner = new Scanner(System.in);
-	private List<String> options = Arrays.asList("Display Order", "Create Order", "Edit Order", "Delete Order", "Exit");
+	private List<String> options = Arrays.asList("Display Available Vehicles", "Display Orders", "Create Order", "Edit Order", "Delete Order", "Exit");
 	
 	
 	public void start() {
@@ -25,13 +26,15 @@ public class Menu {
 			
 			try {
 			if (selection.equals("1")) {
-				displayOrders();
+				displayCars();
 			} else if (selection.equals("2")) {
-				createOrder();
+				displayOrders();
 			} else if (selection.equals("3")) {
-				//editOrder();
+				createOrder();
 			} else if (selection.equals("4")) {
-				//deleteOrder();
+				editOrder();
+			} else if (selection.equals("5")) {
+				deleteOrder();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -39,8 +42,13 @@ public class Menu {
 			System.out.println("Press enter to continue....");
 			scanner.nextLine();
 			
-		} while (!selection.equals("5"));
+		} while (!selection.equals("6"));
 	}
+	
+	//Timestamp
+    Calendar calendar = Calendar.getInstance();
+    java.sql.Timestamp timestamp = new java.sql.Timestamp(calendar.getTime().getTime());
+	
 	
 	private void printMenu() {
 		System.out.println("## Vehicle Purchase Order ##\n\nSelect an option...\n");
@@ -50,62 +58,95 @@ public class Menu {
 	}
 	
 	private void displayCars() throws SQLException {
-		List<Car> cars = carDao.getCars();
+		List<Car> cars = carDao.unsoldCars();
+		if (!cars.isEmpty()) {
 		for (Car car : cars) {
 		System.out.println(carIndex + ". VIN: " + car.getVin() + ", YEAR: " + car.getYear() + ", MAKE: " + car.getMake() + ", MODEL: " + car.getModel() + ", PRICE: $" + car.getPrice());
-		//String s = "VIN: " + car.getVin() + ", YEAR: " + car.getYear() + ", MAKE: " + car.getMake() + ", MODEL: " + car.getModel() + ", PRICE: $" + car.getPrice() + "\n";
 		carIndex++;
-		}
-		carIndex = 1;
+		} 
+			}else {
+			System.out.println("Inventory Empty.\n");
 	}
+		carIndex = 1;
+}
 	
 	private void displayOrders() throws SQLException {
 		List<Order> orders = orderDao.getOrders();
+		if (!orders.isEmpty()) {
 		for (Order order : orders) {
-		System.out.println("ORDER ID: " + order.getOrderId() + ", CUSTOMER ID: " + order.getCustomerId() + ", CAR VIN: " + order.getCarVin() + ", DATE ORDERED: " + order.getDateOrdered() + ", DATE DELIVERED: " + order.getDateDelivered() + "\n");
+		System.out.println("ORDER ID: " + order.getOrderId() + ", CUSTOMER ID: " + order.getCustomerId() + ", CAR VIN: " + order.getCarVin() + ", DATE ORDERED: " + order.getDateOrdered() + "\n");
+		}
+			} else {
+				System.out.println("There are no orders.\n");
+			}
+	}
+	
+	private void displayCustomers() throws SQLException {
+		List<Customer> customers = customerDao.getCustomers();
+		System.out.println("Customers:");
+		for (Customer customer : customers) {
+		System.out.println("ID: " + customer.getCustomerId() + ", FIRST NAME: " + customer.getFirstName() + ", LAST NAME: " + customer.getLastName() + ", STREET: " + customer.getStreet() + ", CITY: " + customer.getCity() + ", STATE: " + customer.getState() + ", ZIP: " + customer.getZip() + "\n");
 		}
 	}
 	
 	private void createOrder() throws SQLException {
-		System.out.println("Enter customer first name:");
-		String firstName = scanner.nextLine();
-		System.out.println("Enter customer last name:");
-		String lastName = scanner.nextLine();
+		System.out.println("**CREATE NEW ORDER**\n");
+		displayCustomers();
+		System.out.println("Choose customer ID:");
+		String customerSelection = scanner.nextLine();
 		List<Car> cars = carDao.getCars();
-		System.out.println("Choose car:");
+		System.out.println("Choose vehicle:");
 		displayCars();
 		String carString = scanner.nextLine();
-		int car = Integer.parseInt(carString);
+		int carChoice = Integer.parseInt(carString)-1;
+		//System.out.println(carChoice);
 		
-		for(int i = car; i < cars.size(); car++) {
-			orderDao.createOrder(customerDao., cars.get(car).getYear(), cars.get(car).getMake(), cars.get(car).getModel(), cars.get(car).getPrice());
+		if (carChoice <= cars.size()-1) {
+			orderDao.createOrder(customerSelection, cars.get(carChoice).getVin(), timestamp.toString(), cars.get(carChoice).getPrice());
+			carDao.soldStatus(true, cars.get(carChoice).getVin());
+		}else {
+			System.out.println("Invalid Choice. Try again.\n");
+			createOrder();
 		}
-		//orderDao.createOrder(customerId, carVin, dateOrdered, dateDelivered, total);
-		System.out.println("Order created");
+		System.out.println("Order created.");
 		displayOrders();
 	}
 	
 	private void editOrder() throws SQLException {
+		System.out.println("Enter Order ID to edit:\n");
 		displayOrders();
-		System.out.println("Enter Order ID:");
-		String stringID = scanner.nextLine();
-		int id = Integer.parseInt(stringID);
-		System.out.println("Enter new customer first name:");
-		String name = scanner.nextLine();
-		System.out.println("Enter new customer last name:");
-		String position = scanner.nextLine();
-		//OrderDao.editOrder(name, position, id);
-		System.out.println("Order succefully edited");
-		displayOrders();		
+		String stringId = scanner.nextLine();
+		int id = Integer.parseInt(stringId);
+		
+		System.out.println("Choose new customer:\n");
+		displayCustomers();
+		String customerId = scanner.nextLine();
+		
+		List<Car> cars = carDao.getCars();
+		System.out.println("Choose new vehicle:\n");
+		displayCars();
+		String carString = scanner.nextLine();
+		int carChoice = Integer.parseInt(carString)-1;
+		System.out.println(carChoice);
+		
+		if (carChoice <= cars.size()-1) {
+			orderDao.editOrder(customerId, cars.get(carChoice).getVin(), timestamp.toString(), cars.get(carChoice).getPrice(), id);
+			carDao.soldStatus(false, cars.get(carChoice).getVin());
+		}else {
+			System.out.println("Invalid Choice. Try again.\n");
+			createOrder();
+		}
+		System.out.println("Order successfully edited\n");
+			displayOrders();
 	}
 	
 	private void deleteOrder() throws SQLException {
+		System.out.println("Enter Order ID to delete\n");
 		displayOrders();
-		System.out.println("Enter order id to delete");
-		String orderID = scanner.nextLine();
-		int id = Integer.parseInt(orderID);
-		orderDao.deleteOrder(id);
-		System.out.println("Order " +  id + " deleted");
+		String orderId = scanner.nextLine();
+		int id = Integer.parseInt(orderId);
+		orderDao.deleteOrder(id);		
+		System.out.println("Order " + id + " deleted\n");
 		displayOrders();
 	}
 
